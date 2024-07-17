30d1dd7f7be6d761e0bbf6cb0a7b7a11d995739e
Rafael-Alexandrini <ralexandrini2@gmail.com>
2024-07-17 17:04:27 -0300

    Melhora desenhador de mapas e outros
    Melhora desenhador de mapas, adiciona mapas e programa que ajuda a configurar o mapa (colocar aspas)

---
+++ RUTD6/construtor-mapa.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main2()
+{
+    char mapa[31][61];
+    printf("Manda la: \n");
+    for(int i = 0; i<31; i++)
+        gets(mapa[i]);
+    for(int l = 0; l< 30; l++)
+    {
+        printf("\"");
+        for (int c = 0; c < 60; c++ )
+        {
+            printf("%c", mapa[l][c]);
+        }
+        printf("\"");
+        printf(",");
+        printf("\n");
+    }
+
+
+
+
+    return 0;
+}
--- RUTD6/main.c
+++ RUTD6/main.c
@@ -4,51 +4,49 @@
 #define ALTURA 600
 #define N_COLUNAS 60
 #define N_LINHAS 30
-#define TAM_QUADRADO 20
 #define TAM_GRID 20
 
+
 int main()
-{
-    char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
+{    char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
-"W                         W                                W",
-"W                         W                                W",
-"W                         W                                W",
-"W                         W                                W",
-"W                         W                                W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
-"W                                                          W",
+"WWWWWWWW                   W         WMW                   W",
+"WWWWWWWW WWWWWWWWWWWWWWWWW W         W W                   W",
+"WWWWWWWW W               W W         WMW                   W",
+"WWWWWWWW H               W W         W W                   W",
+"WWWWWWWW W               W W  R      WMW                   W",
+"WWWWWWWW W               W W         W W            R      W",
+"S        W           R   W W         WMW                   W",
+"WWWWWWWWWW               W W         W W                   W",
+"W                        W W         W W                   W",
+"W                        W W         H H      R            W",
+"W                        W W         W W                   W",
+"W   R                    W W         W W                   W",
+"W                        W W         W WWWWWWWWWWWWHWWWWWWWW",
+"W                        H H         W                     W",
+"W                        W W         W WWWWWWWWWWWWHWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWW WWWW W         W W             WWWWWWW",
+"W             WWWWWWW WWWW W         H H     J       W    WW",
+"W          R  H       WWWW W         W W             W WW WW",
+"W             WWWWWWW WWWW W         W WWWWWWWWWWWWWWW WW WW",
+"W             WWWWWWW WWWW W         W                 WW WW",
+"W      R      WWWWWWW WWWW W         WWWWWWWWWWWWWWWWWWWW WW",
+"W             WWWWWW  WWWW W                            W WW",
+"W        R    WWWWWW WWWWW W                            W WW",
+"W             WWWWWWHWWWWW W                            W WW",
+"W             W          W W                   R        W WW",
+"W   R         W          W WWWWWWHWWWWWWWWWWWWWWWWWWWWWWW WW",
+"W             W          H                                WW",
+"W             W   R      WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
 
     int posX = 300;
     int posY = 300;
-    int jogadorVivo = 1;
 
-    InitWindow(LARGURA, ALTURA, "Quadrado");
+    InitWindow(LARGURA, ALTURA, "RUTD6");
     SetTargetFPS(60);
 
-    while(!WindowShouldClose() && jogadorVivo)
+    while(!WindowShouldClose())
     {
         // Detecta Input
         if (IsKeyPressed(KEY_UP)) posY -= TAM_GRID;
@@ -57,22 +55,27 @@ int main()
         if (IsKeyPressed(KEY_RIGHT)) posX += TAM_GRID;
 
 
-        // Checa se saiu da tela
-        if (posX >= LARGURA || posX < 0 || posY >= ALTURA|| posY < 0)
-        {
-            jogadorVivo = 0;
-        }
-
 
         // Atualiza frame e desenha quadrado
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawRectangle(posX, posY, TAM_QUADRADO, TAM_QUADRADO, GREEN);
         for (int l = 0; l < N_LINHAS; l++){
             for (int c = 0; c < N_COLUNAS; c++){
                 switch(mapa[l][c]){
                     case 'W':
                         DrawRectangle(c * TAM_GRID, l * TAM_GRID, TAM_GRID, TAM_GRID, PURPLE);
+                        break;
+                    case 'H':
+                        DrawRectangle(c * TAM_GRID, l * TAM_GRID, TAM_GRID, TAM_GRID, BLUE);
+                        break;
+                    case 'R':
+                        DrawRectangle(c * TAM_GRID, l * TAM_GRID, TAM_GRID, TAM_GRID, RED);
+                        break;
+                    case 'S':
+                        DrawRectangle(c * TAM_GRID, l * TAM_GRID, TAM_GRID, TAM_GRID, BLACK);
+                        break;
+                    case 'M':
+                        DrawRectangle(c * TAM_GRID, l * TAM_GRID, TAM_GRID, TAM_GRID, YELLOW);
                         break;
                     default:
                         break;
@@ -81,6 +84,7 @@ int main()
 
             }
         }
+        DrawRectangle(posX, posY, TAM_GRID, TAM_GRID, GREEN);
         EndDrawing();
     }
 
@@ -89,3 +93,103 @@ int main()
 
 
 }
+/*
+char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWW     WWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW             H     WWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW WWWWWWWWWWWWW     WWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW WWWWWWWWWWWWW     WWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW WWWWWWWWWWWWWWWHWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWW                W              WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWW       R        W WWWWWHWWWWWW WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWW                W W          W WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWW W          W WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW    WWWW W          W WWWWWWWWWWWWWWWWWWWWW",
+"S               W    W    W          W   WWWWWWWWWWWWWM M MW",
+"WWWWWWWWWWWWWWW W    W WWWW          WWW WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W    W W               W WWWWWWWWWWWWWMWWWWW",
+"WWWWWWWWWWWWWWW W    W H               W WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W    W W      R        W WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W    W W               W WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W  R W W               W WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W    W W               W               WWWWW",
+"WWWWWWWWWWWWWWW W    W W               WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW WWWWWW WWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW        WWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWHWWWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW     R                WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
+
+
+char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W                            W                  W M W      W",
+"W                            W                  H   W      W",
+"W                            W                  W M W      W",
+"W                            H                  W   W      W",
+"W                            W           R      W M W      W",
+"W                            W                  W   W   R  W",
+"W   R                        W                  W M W      W",
+"WWWWWWWWWWWWWWWWWWWHWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   W      W",
+"W                                                 M H      W",
+"WWWWWHWWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWWWWWWHWWWWWWWWW      W",
+"W                              W W                  W      W",
+"W                              W W              R   W      W",
+"W                              W W                  W      W",
+"W                              W W                  H      W",
+"W                  WWWWWWWWWWWWW W                  W      W",
+"W                  W             W                  W R    W",
+"W                  W WWWWWWWWWWWWW                  W      W",
+"WWWWWWWWWWWHWWWWWWWW W                              W      W",
+"W                  W W                              W      W",
+"W                  W W                              W      W",
+"W     R            W WWWWWWWWWWWWWWWWWWWWWWWHWWWWWWWW      W",
+"W                  W W                W             W      W",
+"W                  W W                W             W      W",
+"WWWWWWWWWWWWWWWWWWWW W                W             W      W",
+"S                    W                W             W      W",
+"WWWWWWWWWWHWWWWWWWWWWW                W      R      W      W",
+"W                                     H             W      W",
+"W            R                        W             W      W",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
+
+
+char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWW                   W         WMW                   W",
+"WWWWWWWW WWWWWWWWWWWWWWWWW W         W W                   W",
+"WWWWWWWW W               W W         WMW                   W",
+"WWWWWWWW H               W W         W W                   W",
+"WWWWWWWW W               W W  R      WMW                   W",
+"WWWWWWWW W               W W         W W            R      W",
+"S        W           R   W W         WMW                   W",
+"WWWWWWWWWW               W W         W W                   W",
+"W                        W W         W W                   W",
+"W                        W W         H H      R            W",
+"W                        W W         W W                   W",
+"W   R                    W W         W W                   W",
+"W                        W W         W WWWWWWWWWWWWHWWWWWWWW",
+"W                        H H         W                     W",
+"W                        W W         W WWWWWWWWWWWWHWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWW WWWW W         W W             WWWWWWW",
+"W             WWWWWWW WWWW W         H H     J       W    WW",
+"W          R  H       WWWW W         W W             W WW WW",
+"W             WWWWWWW WWWW W         W WWWWWWWWWWWWWWW WW WW",
+"W             WWWWWWW WWWW W         W                 WW WW",
+"W      R      WWWWWWW WWWW W         WWWWWWWWWWWWWWWWWWWW WW",
+"W             WWWWWW  WWWW W                            W WW",
+"W        R    WWWWWW WWWWW W                            W WW",
+"W             WWWWWWHWWWWW W                            W WW",
+"W             W          W W                   R        W WW",
+"W   R         W          W WWWWWWHWWWWWWWWWWWWWWWWWWWWWWW WW",
+"W             W          H                                WW",
+"W             W   R      WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
+
+*/
