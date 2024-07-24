ea198b5ca1c22200c9a990ba13521ef12decabfc
campossssssss <152921923+campossssssss@users.noreply.github.com>
2024-07-24 20:22:44 -0300

    com texturas, portais, contagem de vida, gameover

--- RUTD6/main.c
+++ RUTD6/main.c
@@ -11,6 +11,7 @@
 
 
 int main()
+<<<<<<< Updated upstream
 {    int l, c, i;
     char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
@@ -42,6 +43,42 @@ int main()
 "W   R         W          W WWWWWWHWWWWWWWWWWWWWWWWWWWWWWW WW",
 "W             W          H                                WW",
 "W             W   R      WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+=======
+{   int l, c, i, b;
+    Color cinzamorto = {176, 176, 176, 200};
+    Color caminho = {11, 140, 133, 255};
+
+char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W        WWWW  WWWWW   WWWW                                W",
+"W    R   WWWW  WWWWW   WWWW                                W",
+"W        H         H   WWWW                        R       W",
+"W        WWWW  WWWWW   WWWW                                W",
+"W  R     WWWW  WWWWW   WWWW                                W",
+"W        WWWW  WWWWW   WWWW         R                      W",
+"W        WWWW  WWWWW                               J       W",
+"W      R WWWW  WWWWW   WWWW                                W",
+"W        WWWW  WWWWW   WWWW                                W",
+"W        WWWW  WWWWWWWWWWWW                R               W",
+"W        WWWW  W          W                                W",
+"W     R  WWWW  W WWWWWWWW W                                W",
+"W        WWWW  W W      W W                                W",
+"W        WWWW  W W      W WWWWWWWWWWWWWWWWW         R      W",
+"W   R    WWWW  W W      W                 W                W",
+"W        WWWW  W W   R  WWWWWWWWWWWWWWWWW WWW           WWWW",
+"W        WWWW                           W WWWW         WWWWW",
+"W  R     WWWW  W W                      W WWWWW       WWWWWW",
+"W        WWWW  W W                      W WWWWWW     WWWWWWW",
+"W      R WWWW  W W              R       W WWWWWWW   WWWWWWWW",
+"W        WWWW  W W     R                W WWWWWWWW WWWWWWWWW",
+"W        WWWW  W W                      W WWWWWWWW WWWWWWWWW",
+"W        WWWWWWW W                      W WWWWWWWW WWWWWWWWW",
+"W        WM M    W                      W WWWWWWWWHWWWWWWWWW",
+"W        W WWWWWWW          R                              S",
+"W   R    WMWWWWWWW                      WWWWWWWWWWWWWWWWWWWW",
+"W        W WWWWWWW                                 WWWWWWWWW",
+"W        WMWWWWWWW                                 WWWWWWWWW",
+>>>>>>> Stashed changes
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
 
     struct posicao player = {15, 15};
@@ -62,18 +99,37 @@ int main()
     while(!WindowShouldClose())
     {
 
+<<<<<<< Updated upstream
 
         // Movimento do Jogador  // Remover coisa do shift antes de lançar
         if (IsKeyDown(KEY_LEFT_SHIFT)){
+=======
+        if (gameover == 1)
+        {
+            if (IsKeyDown(KEY_LEFT_SHIFT))
+            {
+>>>>>>> Stashed changes
             if (IsKeyDown(KEY_UP)) tenta_mover(&player.x, &player.y, 0, -1, mapa, N_LINHAS, N_COLUNAS);
             if (IsKeyDown(KEY_DOWN)) tenta_mover(&player.x, &player.y, 0, 1, mapa, N_LINHAS, N_COLUNAS);
             if (IsKeyDown(KEY_LEFT)) tenta_mover(&player.x, &player.y, -1, 0, mapa, N_LINHAS, N_COLUNAS);
             if (IsKeyDown(KEY_RIGHT)) tenta_mover(&player.x, &player.y, 1, 0, mapa, N_LINHAS, N_COLUNAS);
+<<<<<<< Updated upstream
         } else{
             if (IsKeyPressed(KEY_UP)) tenta_mover(&player.x, &player.y, 0, -1, mapa, N_LINHAS, N_COLUNAS);
             if (IsKeyPressed(KEY_DOWN)) tenta_mover(&player.x, &player.y, 0, 1, mapa, N_LINHAS, N_COLUNAS);
             if (IsKeyPressed(KEY_LEFT)) tenta_mover(&player.x, &player.y, -1, 0, mapa, N_LINHAS, N_COLUNAS);
             if (IsKeyPressed(KEY_RIGHT)) tenta_mover(&player.x, &player.y, 1, 0, mapa, N_LINHAS, N_COLUNAS);
+=======
+            }
+            else
+            {
+                if (IsKeyPressed(KEY_UP)) tenta_mover(&player.x, &player.y, 0, -1, mapa, N_LINHAS, N_COLUNAS);
+                if (IsKeyPressed(KEY_DOWN)) tenta_mover(&player.x, &player.y, 0, 1, mapa, N_LINHAS, N_COLUNAS);
+                if (IsKeyPressed(KEY_LEFT)) tenta_mover(&player.x, &player.y, -1, 0, mapa, N_LINHAS, N_COLUNAS);
+                if (IsKeyPressed(KEY_RIGHT)) tenta_mover(&player.x, &player.y, 1, 0, mapa, N_LINHAS, N_COLUNAS);
+            }
+
+>>>>>>> Stashed changes
         }
 
 
@@ -97,72 +153,73 @@ int main()
 
 
 /*
+mapa 1
 char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWW     WWWWWWWWWWWWWWWWWWWWWWWWWW",
-"WWWWWWWWWWWWWWW             H     WWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWW                   WWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWW WWWWWWWWWWWWW     WWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWW WWWWWWWWWWWWW     WWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWW WWWWWWWWWWWWWWWHWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWW                W              WWWWWWWWWWWWWWWWWWWWW",
-"WWWWWWWW       R        W WWWWWHWWWWWW WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWW       R        W WWWWW WWWWWW WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWW                W W          W WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWWWWWWWWWW W          W WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWW    WWWW W          W WWWWWWWWWWWWWWWWWWWWW",
-"S               W    W    W          W   WWWWWWWWWWWWWM M MW",
+"S                    W    W          W   WWWWWWWWWWWWWM M MW",
 "WWWWWWWWWWWWWWW W    W WWWW          WWW WWWWWWWWWWWWW WWWWW",
 "WWWWWWWWWWWWWWW W    W W               W WWWWWWWWWWWWWMWWWWW",
-"WWWWWWWWWWWWWWW W    W H               W WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W    W W               W WWWWWWWWWWWWW WWWWW",
 "WWWWWWWWWWWWWWW W    W W      R        W WWWWWWWWWWWWW WWWWW",
 "WWWWWWWWWWWWWWW W    W W               W WWWWWWWWWWWWW WWWWW",
-"WWWWWWWWWWWWWWW W  R W W               W WWWWWWWWWWWWW WWWWW",
+"WWWWWWWWWWWWWWW W  R                     WWWWWWWWWWWWW WWWWW",
 "WWWWWWWWWWWWWWW W    W W               W               WWWWW",
 "WWWWWWWWWWWWWWW W    W W               WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWW WWWWWW WWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWW        WWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
-"WWWWWWWWWWWWWWWWWWHWWWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWW     R                WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
-"WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWHWWWWWW WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWW                      WWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
 
-
+mapa 2
 char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "W                            W                  W M W      W",
-"W                            W                  H   W      W",
+"W                            W                  W   W      W",
 "W                            W                  W M W      W",
-"W                            H                  W   W      W",
+"W               J            H                  W   H      W",
 "W                            W           R      W M W      W",
 "W                            W                  W   W   R  W",
 "W   R                        W                  W M W      W",
-"WWWWWWWWWWWWWWWWWWWHWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   W      W",
-"W                                                 M H      W",
-"WWWWWHWWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWWWWWWHWWWWWWWWW      W",
-"W                              W W                  W      W",
-"W                              W W              R   W      W",
-"W                              W W                  W      W",
-"W                              W W                  H      W",
-"W                  WWWWWWWWWWWWW W                  W      W",
-"W                  W             W                  W R    W",
-"W                  W WWWWWWWWWWWWW                  W      W",
-"WWWWWWWWWWWHWWWWWWWW W                              W      W",
+"WWWWWWWWWWW   WWWWWWWWWWWWWWWWWWWWWWWWWWW WWWHWWW   W      W",
+"W                                                 M W      W",
+"WWWWWWWWWWW   WWWWWWWWWWWWWWWWWW WWWWWWWW WWWWWWWWWWW      W",
+"WWWWWWWWWW    WWWWWWWWWWWWWWWWWW W                  W      W",
+"WWWWWWWW     WWWWWWWWWWWWWWWWWWW W              R   W      W",
+"WWWWWWW    WWWWWWWWWWWWWWWWWWWWW W                  W      W",
+"WWWWWWW   WWWWWWWWWWWWWWWWWWWWWW W                  W      W",
+"WWWWWWW   WWWWWWWWWWWWWWWWWWWWWW W                         W",
+"WWWWWW   WWWWWWWWWWW             W                    R    W",
+"WWWWWW   WWWWWWWWWWW WWWWWWWWWWWWW                         W",
+"WWWWWW   WWWWWWWWWWW W                              W      W",
 "W                  W W                              W      W",
 "W                  W W                              W      W",
-"W     R            W WWWWWWWWWWWWWWWWWWWWWWWHWWWWWWWW      W",
+"W      R           W WWWWWWWWWWWWWWWWWWWWWWWWHWWWWWWW      W",
 "W                  W W                W             W      W",
-"W                  W W                W             W      W",
-"WWWWWWWWWWWWWWWWWWWW W                W             W      W",
+"W                                     W             W      W",
+"WWWWWWWWWWWHWWWWWWWW W                W             W      W",
 "S                    W                W             W      W",
-"WWWWWWWWWWHWWWWWWWWWWW                W      R      W      W",
-"W                                     H             W      W",
+"WWWWWWWWWWWHWWWWWWWWWW                       R      W      W",
+"W                                     W             W      W",
 "W            R                        W             W      W",
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
 
-
+mapa 3
 char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWW                   W         WMW                   W",
@@ -195,4 +252,36 @@ char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
 "W             W   R      WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
 
+mapa 4
+char mapa[N_LINHAS + 1][N_COLUNAS + 1] = {
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W        WWWW  WWWWW   WWWW                                W",
+"W    R   WWWW  WWWWW   WWWW                                W",
+"W        H         H   WWWW                        R       W",
+"W        WWWW  WWWWW   WWWW                                W",
+"W  R     WWWW  WWWWW   WWWW                                W",
+"W        WWWW  WWWWW   WWWW         R                      W",
+"W        WWWW  WWWWW                               J       W",
+"W      R WWWW  WWWWW   WWWW                                W",
+"W        WWWW  WWWWW   WWWW                                W",
+"W        WWWW  WWWWWWWWWWWW                R               W",
+"W        WWWW  W          W                                W",
+"W     R  WWWW  W WWWWWWWW W                                W",
+"W        WWWW  W W      W W                                W",
+"W        WWWW  W W      W WWWWWWWWWWWWWWWWW         R      W",
+"W   R    WWWW  W W      W                 W                W",
+"W        WWWW  W W   R  WWWWWWWWWWWWWWWWW WWW           WWWW",
+"W        WWWW                           W WWWW         WWWWW",
+"W  R     WWWW  W W                      W WWWWW       WWWWWW",
+"W        WWWW  W W                      W WWWWWW     WWWWWWW",
+"W      R WWWW  W W              R       W WWWWWWW   WWWWWWWW",
+"W        WWWW  W W     R                W WWWWWWWW WWWWWWWWW",
+"W        WWWW  W W                      W WWWWWWWW WWWWWWWWW",
+"W        WWWWWWW W                      W WWWWWWWW WWWWWWWWW",
+"W        WM M    W                      W WWWWWWWWHWWWWWWWWW",
+"W        W WWWWWWW          R                              S",
+"W   R    WMWWWWWWW                      WWWWWWWWWWWWWWWWWWWW",
+"W        W WWWWWWW                                 WWWWWWWWW",
+"W        WMWWWWWWW                                 WWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"};
 */
--- RUTD6/nossas_funcoes.h
+++ RUTD6/nossas_funcoes.h
@@ -8,8 +8,12 @@ void funcao_exemplo();
 void desenha_mapa(char mapa[31][61], int nLinhas, int nColunas, int tamGrid);
 
 void tenta_mover(int *pX, int *pY, int dirX, int dirY, char mapa[31][61], int nLinhas, int nColunas);
+<<<<<<< Updated upstream
 
 void entra_portal(int *pX, int *pY, int dirX, int dirY, char mapa[31][61], int nLinhas, int nColunas);
+=======
+>>>>>>> Stashed changes
 
+void entra_portal(int *pX, int *pY, int dirX, int dirY, char mapa[31][61], int nLinhas, int nColunas);
 
 #endif // NOSSAS_FUNCOES_H_
